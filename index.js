7f6ec94b668d79d7d558aeb8e1f912bb8167d737
PiotrSitarek <piotr.sitarek.05@gmail.com>
2020-05-26 15:30:54 +0200

    page upgrade

--- index.js
+++ index.js
@@ -1,77 +1,79 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOMContentLoaded")
+});
 
-    fetch(`https://restcountries.eu/rest/v2/all`)
-        .then(response => response.json())
-        .then(response => createCountryList(response))
-        .catch(error => console.log(error));
+fetch(`https://restcountries.eu/rest/v2/all`)
+    .then(response => response.json())
+    .then(response => createCountryList(response))
+    .catch(error => console.log(error));
 
-    let countryArray = [];
+let countryArray = [];
 
-    const createCountryList = (response) => {
-        response.forEach(function (element) {
-            countryArray.push(element)
-        });
-    };
+const createCountryList = (response) => {
+    response.forEach(function (element) {
+        countryArray.push(element)
+    });
+};
+
+const mainContainer = document.querySelector(".mainContainer");
+
+const countryList = document.createElement("ul");
+countryList.classList.add("countryList");
+
+const countryInfo = document.createElement("div");
+countryInfo.classList.add("infoContainer");
+
+const displayButton = document.querySelector(".displayButton");
+
+displayButton.addEventListener("click", function () {
+
+    displayButton.setAttribute("disabled", "true")
+
+    mainContainer.appendChild(countryInfo);
+    const infoContainer = document.querySelector(".infoContainer");
+
+    mainContainer.appendChild(countryList);
+    const countryUL = document.querySelector(".countryList");
+
+    countryArray.map(element => {
+
+        const newListElement = document.createElement("li");
+        countryUL.appendChild(newListElement);
+        newListElement.classList.add("newListElement")
+        newListElement.innerHTML = element.name;
+
+        const dataDisplay = () => {
+
+            const flag = document.createElement("img");
+            const countryName = document.createElement("h3");
+            const capital = document.createElement("p");
+            const region = document.createElement("p");
+            const population = document.createElement("p");
+
+            infoContainer.appendChild(flag);
+            infoContainer.appendChild(countryName);
+            infoContainer.appendChild(capital);
+            infoContainer.appendChild(region);
+            infoContainer.appendChild(population);
 
 
-    const mainContainer = document.querySelector(".mainContainer"); // głowny przy którym trzeba stworzyć 2 divy
+            flag.classList.add("flagImage");
+            const flagSrc = document.querySelector(".flagImage");
+            flagSrc.setAttribute("src", `${element.flag}`);
+            countryName.innerHTML = element.name;
+            capital.innerHTML = "Capital: " + element.capital;
+            region.innerHTML = "Region: " + element.subregion;
+            population.innerHTML = "Population: " + element.population;
 
-    const countryList = document.createElement("ul"); // czerwony
-    countryList.classList.add("countryList"); // style czerwonego
+        }
 
-    const countryInfo = document.createElement("div"); // niebieski
-    countryInfo.classList.add("infoContainer"); // style niebieskiego
-
-    const displayButton = document.querySelector(".displayButton"); // złapanie buttonu Display
-
-    displayButton.addEventListener("click", function () {
-
-        mainContainer.appendChild(countryInfo); //utworzenie niebieskiego
-        const infoContainer = document.querySelector(".infoContainer"); // złapanie niebieskiego
-
-        mainContainer.appendChild(countryList); // utworzenie czerwonego
-        const countryUL = document.querySelector(".countryList"); // złapanie czerwonego
-
-        countryArray.map(element => {
-
-            const newListElement = document.createElement("li");
-            countryUL.appendChild(newListElement);
-            newListElement.innerHTML = element.name;
-
-            const dataDisplay = () => {
-
-                const flag = document.createElement("img");
-                const countryName = document.createElement("h3");
-                const capital = document.createElement("p");
-                const region = document.createElement("p");
-                const population = document.createElement("p");
-
-                infoContainer.appendChild(flag);
-                infoContainer.appendChild(countryName);
-                infoContainer.appendChild(capital);
-                infoContainer.appendChild(region);
-                infoContainer.appendChild(population);
-
-
-                flag.classList.add("flagImage");
-                const flagSrc = document.querySelector(".flagImage");
-                flagSrc.setAttribute("src", `${element.flag}`);
-                countryName.innerHTML = element.name;
-                capital.innerHTML = "Capital: " + element.capital;
-                region.innerHTML = "Region: " + element.subregion;
-                population.innerHTML = "Population: " + element.population;
-
+        newListElement.addEventListener("click", function () {
+            if (infoContainer.childElementCount === 0) {
+                dataDisplay();
+            } else {
+                infoContainer.innerHTML = "";
+                dataDisplay();
             }
-
-            newListElement.addEventListener("click", function () {
-                if (infoContainer.childElementCount === 0) {
-                    dataDisplay();
-                } else {
-                    infoContainer.innerHTML = "";
-                    dataDisplay();
-                }
-            });
         });
     });
 });
--- quiz.js
+++ quiz.js
@@ -1,3 +1,103 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOMContentLoaded")
 });
+
+fetch(`https://restcountries.eu/rest/v2/all`)
+    .then(response => response.json())
+    .then(response => createCountryList(response))
+    .catch(error => console.log(error));
+
+let countryArray = [];
+
+const createCountryList = (response) => {
+    response.forEach(function (element) {
+        countryArray.push(element)
+    });
+};
+
+function drawRandomIndex() {
+    min = Math.ceil(0);
+    max = Math.floor(249);
+    return Math.floor(Math.random() * (249 - 0 + 1)) + 0;
+}
+let counter = 0;
+
+const startButton = document.querySelector(".startButton");
+const imageAnswerContainer = document.querySelector(".imageAnswerContainer");
+const quizInfoContainer = document.querySelector(".quizInfoContainer");
+
+startButton.addEventListener("click", function () {
+
+    let flagIndex = drawRandomIndex();
+    let randomFirst = drawRandomIndex();
+    let randomSecond = drawRandomIndex();
+
+
+    if (counter > 0) {
+        startButton.innerHTML = "Graj dalej!"
+    }
+    const drawFunction = () => {
+
+        const createQuestion = () => {
+            const quizFlag = document.createElement("img");
+            imageAnswerContainer.appendChild(quizFlag);
+
+            quizFlag.classList.add("flagImage");
+            const flagSrc = document.querySelector(".flagImage");
+            flagSrc.setAttribute("src", `${countryArray[flagIndex].flag}`);
+        }
+        createQuestion();
+
+        let quizAnswers = [countryArray[flagIndex], countryArray[randomFirst], countryArray[randomSecond]];
+
+        const sortByPopulation = (a, b) => {
+            return a.population - b.population;
+        }
+        quizAnswers.sort(sortByPopulation);
+
+
+        const propmt = document.createElement("p");
+        imageAnswerContainer.appendChild(propmt);
+        propmt.classList.add("proptStyle")
+        propmt.innerHTML = "Click on your answer";
+        startButton.innerHTML = "Draw!";
+
+        quizAnswers.map(element => {
+            const answerElement = document.createElement("p")
+            imageAnswerContainer.appendChild(answerElement);
+            answerElement.classList.add("answerElement")
+            answerElement.innerHTML = element.name;
+
+            answerElement.addEventListener("click", function () {
+                if (element.name === countryArray[flagIndex].name) {
+                    counter++;
+                    alert("Correct!");
+                    imageAnswerContainer.innerHTML = "";
+                    createQuestion();
+                    if (counter > 0) {
+                        startButton.innerHTML = "Continue playing!";
+                    }
+
+                    if (counter === 3) {
+                        alert("Congrats You win! You are good in this game!");
+                        imageAnswerContainer.innerHTML = "";
+                        counter = 0;
+                        startButton.innerHTML = "Play once again!";
+                    }
+                } else {
+                    counter = 0;
+                    alert("You loose!");
+                    startButton.innerHTML = "Play once again!";
+                    imageAnswerContainer.innerHTML = "";
+                }
+            });
+        });
+    };
+
+    if (imageAnswerContainer.childElementCount === 0) {
+        drawFunction();
+    } else {
+        imageAnswerContainer.innerHTML = "";
+        drawFunction();
+    }
+});
